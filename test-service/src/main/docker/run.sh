411b9c5c721fb9cc53cf26ea65034231e51a08b7
polymorphicfox <kostinfw@gmail.com>
2019-08-01 23:53:22 +0400

    init

---
+++ test-service/src/main/docker/run.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+echo "********************************************************"
+echo "Starting test-service "
+echo "********************************************************"
+java -jar /usr/local/test/test-service/@project.build.finalName@.jar
